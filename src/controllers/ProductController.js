1481889326ce6c79072c1212d28d4d31382287c5
Sergio Mirapalhete <sergio.mirapalhete@operacao.rcadigital.com.br>
2021-06-10 00:33:56 -0300

    ref: validations and exceptions

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,55 +1,127 @@
 const Product = require("../model/Product");
 const Category = require("../model/Category");
+function productValidation(data){
+  let errors = [];
+  if(!data || !data.title){
+    errors.push('Title is mandatory!');
+  }
+  if(!data || !data.description){
+    errors.push('Description is mandatory!');
+  }
+  if(!data || !data.price){
+    errors.push('Price is mandatory!');
+  }
+  if(!data || !data.category){
+    errors.push('Category is mandatory!');
+  }
+  return errors;
+}
 
 class ProductController {
+  
+
   async store(req, res) {
     try {
       const {title, description, price, category} = req.body;
-      const categoryResult = await Category.find({name: category});
+      const errors = productValidation(req.body);
+      if(errors.length){
+        
+        return res.status(400).json({
+          message: errors.join(' ')
+        })
+      }
+      const categoryExists = await Category.find({name: category.trim()});
+      if(!(categoryExists && categoryExists.length)){
+        return res.status(404).json({
+          message: `Category ${category} not found.`
+        })
+      }
+      const productExists = await Product.find({
+        title: title.trim(),
+        description: description.trim(),
+        price: price
+      });
+      if(productExists && productExists.length){
+        return res.status(409).json({
+          message: `Product ${title} already exists.`
+        });
+      }
       const data = await Product.create({
-        title: title, 
-        description: description, 
+        title: title.trim(), 
+        description: description.trim(), 
         price, 
-        category: categoryResult[0]._id
+        category: categoryExists[0]._id
       });
       return res.status(201).json(data);
     } catch (error) {
-      return res.status(500).json({message: 'Internal error.'});
+      return res.status(error.status || 500).json({
+        message: error.message || 'Internal error.'
+      });
     }
   }
 
   async index(req, res) {
     try {
       const data = await Product.find().populate('category');
-      return res.json(data);
+      return res.status(200).json({
+        products: data, 
+        total: data.length
+      });
     } catch (error) {
-      return res.status(500).json({ message: 'Internal error.'});
+      return res.status(error.status || 500).json({
+        message: error.message || 'Internal error.'
+      });
     }
+
   }
 
   async update(req, res) {
     const { id } = req.params;
     const { title, description, price, category } = req.body;
     const product = await Product.findById(id);
+    if(!product) {
+      return res.status(404).json({message: 'Product not found.'});
+    }
     try {
+      if(category) {
+        const result = await Category.findOne({ name: category.trim() });
+        if(result) {
+          await Product.findByIdAndUpdate(id, { 
+            title: title.trim() || product.title, 
+            description: description.trim() || product.description, 
+            price: price || product.price, 
+            category: result._id 
+          });
+          return res.json({message: 'Product up to date.'})
+        }
+        return res.status(404).json({message: `Category ${category} not found.`});
+      }
       await Product.findByIdAndUpdate(id, { 
-        title: title || product.title, 
-        description: description || product.description, 
+        title: title.trim() || product.title, 
+        description: description.trim() || product.description, 
         price: price || product.price,
       });
       return res.json({message: 'Product up to date.'})
     } catch (error) {
-      return res.status(error.status || 500).json({message: 'Internal error.'});
+      return res.status(error.status || 500).json({
+        message: error.message || 'Internal error.'
+      });
     }
   }
 
   async delete(req, res) {
     const { id } = req.params;
+    const product = await Product.findById(id);
+    if(!product) {
+      return res.status(404).json({message: `Product not found.`});
+    }
     try {
       await Product.findByIdAndDelete(id);
       return res.json({message: 'Product deleted.'})
     } catch (error) {
-      return res.status(500).json({message: 'Internal error.'});
+      return res.status(error.status || 500).json({
+        message: error.message || 'Internal error.'
+      });
     }
   }
 
@@ -63,14 +135,18 @@ class ProductController {
           return res.json(products);
         }
         return res.status(404).json({message: 'Category not found.'});
+      }else{
+        const data = await Product.find({
+          'title' : name
+        }, {
+          __v: 0
+        }).populate('category');
+        return res.status(200).json(data);
       }
-      const data = await Product.find({
-        'title' : name
-      }).populate('category');
-      return res.json(data);
-    
     } catch (error) {
-      return res.status(500).json({message: 'Internal error.'});
+      return res.status(error.status || 500).json({
+        message: error.message || 'Internal error.'
+      });
     }
   }
 }
